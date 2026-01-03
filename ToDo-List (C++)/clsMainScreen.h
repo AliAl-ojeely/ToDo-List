8d0ec652b79d07f0919ea6c4c9e066e6a30ef65d
AliAl-ojeely <alialojeely@gmail.com>
2026-01-03 18:24:02 +0300

    Add project files.

---
+++ ToDo-List (C++)/clsMainScreen.h
@@ -0,0 +1,159 @@
+#pragma once
+#include <iostream>
+#include "clsScreen.h"
+#include "clsInputValidate.h"
+
+using namespace std;
+
+class clsMainScreen :protected clsScreen
+{
+
+private:
+    enum enMainMenueOptions {
+        eListWorks = 1, eAddNewWork = 2, eDeleteWork = 3,
+        eUpdateWork = 4, eFindWork = 5, eWorkRegister = 6, eExit = 7
+    };
+
+    static short _ReadMainMenueOption()
+    {
+        cout << setw(37) << left << "" << "Choose what do you want to do? [1 to 7]? ";
+        short Choice = clsInputValidate::ReadShortNumberBetween(1, 7, "Enter Number Between 1 to 7? ");
+        return Choice;
+    }
+
+    static void _GoBackToMainMenue()
+    {
+        cout << setw(37) << left << "" << "\n\tPress any key to go back to Main Menue...\n";
+
+        system("pause>0");
+        ShowMainMenue();
+    }
+
+    static void _ShowAllWorksScreen()
+    {
+        cout << "\nWork List Screen will be here...\n";
+        
+
+    }
+
+    static void _ShowAddNewWorkScreen()
+    {
+        cout << "\nAdd New Work Screen will be here...\n";
+        
+
+    }
+
+    static void _ShowDeleteWorkScreen()
+    {
+        cout << "\nDelete Work Screen will be here...\n";
+       
+
+    }
+
+    static void _ShowUpdateWorkScreen()
+    {
+        cout << "\nUpdate Work Screen will be here...\n";
+        
+
+    }
+
+    static void _ShowFindWorkScreen()
+    {
+        cout << "\nFind Work Screen will be here...\n";
+        
+
+    }
+
+
+    static void _ShowWorkRegister()
+    {
+        cout << "\n Work Register Screen will be here...\n";
+        
+    }
+
+
+    static void _PerformMainMenueOption(enMainMenueOptions MainMenuOption)
+    {
+        switch (MainMenuOption)
+        {
+        case enMainMenueOptions::eListWorks:
+        {
+            system("cls");
+            _ShowAllWorksScreen();
+            _GoBackToMainMenue();
+            break;
+        }
+
+        case enMainMenueOptions::eAddNewWork:
+        {
+            system("cls");
+            _ShowAddNewWorkScreen();
+            _GoBackToMainMenue();
+            break;
+        }
+
+        case enMainMenueOptions::eDeleteWork:
+        {
+            system("cls");
+            _ShowDeleteWorkScreen();
+            _GoBackToMainMenue();
+            break;
+        }
+
+        case enMainMenueOptions::eUpdateWork:
+        {
+            system("cls");
+            _ShowUpdateWorkScreen();
+            _GoBackToMainMenue();
+            break;
+        }
+
+        case enMainMenueOptions::eFindWork:
+        {
+            system("cls");
+            _ShowFindWorkScreen();
+            _GoBackToMainMenue();
+            break;
+        }
+
+        case enMainMenueOptions::eWorkRegister:
+        {
+            system("cls");
+            _ShowWorkRegister();
+            _GoBackToMainMenue();
+            break;
+        }
+
+        case enMainMenueOptions::eExit:
+        {
+            exit(0);
+            break;
+        }
+
+        }
+    }
+
+public:
+
+    static  void ShowMainMenue()
+    {
+        system("cls");
+        _DrawScreenHeader("\t\tMain Screen");
+
+        cout << setw(37) << left << "" << "===========================================\n";
+        cout << setw(37) << left << "" << "\t\t\tMain Menue\n";
+        cout << setw(37) << left << "" << "===========================================\n";
+        cout << setw(37) << left << "" << "\t[1] Show To-Do List.\n";
+        cout << setw(37) << left << "" << "\t[2] Add New Work.\n";
+        cout << setw(37) << left << "" << "\t[3] Delete Work.\n";
+        cout << setw(37) << left << "" << "\t[4] Update Work Info.\n";
+        cout << setw(37) << left << "" << "\t[5] Find Work In List.\n";
+        cout << setw(37) << left << "" << "\t[6] Work Register.\n";
+        cout << setw(37) << left << "" << "\t[7] Exit.\n";
+        cout << setw(37) << left << "" << "===========================================\n";
+
+        _PerformMainMenueOption((enMainMenueOptions)_ReadMainMenueOption());
+    }
+
+};
+
